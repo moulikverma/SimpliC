b207c363f796a9c130733ea7926d7531d5722ba6
rahulchy960 <rahulchy960@gmail.com>
2024-09-16 09:22:02 -0700

    The main.c is ready for testing the tokenizer

--- src/main.c
+++ src/main.c
@@ -1,4 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "include/lexer.h"
 int main(int argc, char* argv[]){
-    printf("Hello World!\n");
+    lexer_T* lexer = init_lexer(
+        "var name = \"Rahul Chowdhury\";\n"
+        "print(name);\n"
+    );
+    token_T* token = NULL;
+    while((token = lexer_get_next_token(lexer)) != NULL){
+        printf("TOKEN(%d, %s)\n", token->type, token->value);
+    }
+    return 0;
 }
